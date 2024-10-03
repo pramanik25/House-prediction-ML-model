ccf6ead84414aeda8afce011be88ec2951583c20
Vikash Pramanik <vikashpramanikofficial@gmail.com>
2024-10-03 23:54:54 +0530

    changes saved

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -30,7 +30,7 @@ class DataTransformation:
 
             catigorical_features =['Location']
 
-            numerical_features = ['id','Date','number of bedrooms','number of bathrooms','living area','lot area','number of floors','waterfront present','number of views','condition of the house','grade of the house','Area of the house(excluding basement)','Area of the basement','Built Year','Renovation Year','Postal Code','Lattitude','Longitude','living_area_renov','lot_area_renov','Number of schools nearby','Distance from the airport','Price']
+            numerical_features = ['Date','number of bedrooms','number of bathrooms','living area','lot area','number of floors','waterfront present','number of views','condition of the house','grade of the house','Area of the house(excluding basement)','Area of the basement','Built Year','Renovation Year','Postal Code','Lattitude','Longitude','living_area_renov','lot_area_renov','Number of schools nearby','Distance from the airport','Price']
 
             # numerical Pipline
 
